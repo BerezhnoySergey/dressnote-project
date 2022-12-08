bd83ad4a7eb3b163c2c5c4d37fd592e884560f2e
sergey berezhnoy <berezhnoy.sergey550@gmail.com>
2022-12-08 10:39:30 +0200

    add preloader

--- src/loader/Loader.jsx
+++ src/loader/Loader.jsx
@@ -3,13 +3,11 @@ import React from "react";
 import "../components/newIn/newIn.scss";
 const Loader = () => {
 	return (
-		<div className="loader__wrapp">
-			<div class="lds-ring ">
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-			</div>
+		<div class="lds-ring ">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
 		</div>
 	);
 };
